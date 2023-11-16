46dc85a43b46f093ab292468fefad7c9735efc27
YeongminKim <misery010101@gmail.com>
2023-11-17 03:40:10 +0900

    edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { motion } from 'framer-motion';
 
 import { pageEffect } from '@/components/common/framer/pageEffect';
-import type { IFaqProps } from '@/components/faq/Faq';
 import { fetchFaqData } from '@/components/faq/Faq';
 import { Layout } from '@/components/layout/Layout';
 import { LayoutEmpty } from '@/components/layout/LayoutEmpty';
 import Main from '@/components/main';
 
-const Index = ({ faqData }: IFaqProps) => (
+const faqData = await fetchFaqData();
+// const Index = ({ faqData }: IFaqProps) => (
+
+const Index = () => (
   <LayoutEmpty>
     <Layout>
       <motion.div initial="initial" animate="in" exit="out" transition={{ duration: 0.8 }} variants={pageEffect}>
@@ -17,13 +19,13 @@ const Index = ({ faqData }: IFaqProps) => (
   </LayoutEmpty>
 );
 
-export const getServerSideProps = async () => {
-  const faqData = await fetchFaqData();
-  return {
-    props: {
-      faqData,
-    },
-  };
-};
+// export const getServerSideProps = async () => {
+//   const faqData = await fetchFaqData();
+//   return {
+//     props: {
+//       faqData,
+//     },
+//   };
+// };
 
 export default Index;
